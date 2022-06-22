60fc37dc7a4c4952a66ee7895461534a8c373fd6
lindellcarternyc <lindellcarternyc@gmail.com>
2022-06-22 12:46:30 -0400

    adds HMSRoomProvider to app root

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { HMSRoomProvider } from '@100mslive/react-sdk'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <HMSRoomProvider>
+      <App />
+    </HMSRoomProvider>
   </React.StrictMode>
 );
 
